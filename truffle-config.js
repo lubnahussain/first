3ef75edabd722163c73c86e27e8426a5514cca7f
lubnahussain <80570559+lubnahussain@users.noreply.github.com>
2021-03-16 22:53:50 +0500

    commit

---
+++ truffle-config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const infuraKey = "f8daf0fa9e9b4836a590cf1c1398b05e";
+//
+const fs = require('fs');
+//Add .secret file in root folder and place your HD wallet's mnemonic in that file
+// e.g mnemonic of MetaMask wallet
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    develop: {
+      port: 8545
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      //confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      //timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      //skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+  }
+};
